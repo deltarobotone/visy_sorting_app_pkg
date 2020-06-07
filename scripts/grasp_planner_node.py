f993274755a2a55da9cd3a75826a4f9630f66244
buerger-pascal <buerger.pascal@web.de>
2020-06-07 16:53:54 +0200

    first running app grasp some chips

--- scripts/grasp_planner_node.py
+++ scripts/grasp_planner_node.py
@@ -35,20 +35,20 @@ class SortingAppNode:
 
     def __metalChipCB(self,metalChipMsg):
         rospy.loginfo("callback")
-        if self.__start == True and len(self.__metalChips) < 10:
+        if self.__start == True and len(self.__metalChips) < 30:
             if metalChipMsg.pos > self.__metalChipLast.pos:
                 self.__metalChips.append(metalChipMsg)
                 self.__metalChipLast = metalChipMsg
             else:
                 self.__reset()
 
-        if len(self.__metalChips) >= 10:
+        if len(self.__metalChips) >= 30:
             vel = 0
             for metalChip in self.__metalChips:
                 vel = vel + metalChip.vel
             vel = vel/len(self.__metalChips)
 
-            time = (4000 - self.__metalChipLast.pos)/(vel*1000.0)
+            time = ((4000 - self.__metalChipLast.pos)/(vel*1000.0))/10.0
             rospy.loginfo("pos")
             rospy.loginfo(self.__metalChipLast.pos)
 
@@ -60,9 +60,19 @@ class SortingAppNode:
 
             rospy.sleep(time)
 
-            if self.__metalChipLast.hue == 1: self.__pick_and_place_cli(PickAndPlaceRequest.CASE_1)
-            if self.__metalChipLast.hue == 2: self.__pick_and_place_cli(PickAndPlaceRequest.CASE_2)
-            if self.__metalChipLast.hue == 3: self.__pick_and_place_cli(PickAndPlaceRequest.CASE_3)
+            case = 0
+            hue = self.__metalChipLast.hue
+            #case=1 rot
+            if ((hue >= 0 and hue < 10) or (hue <= 180 and hue > 160)): case = 1
+            #case=2 gelb
+            if (hue >= 15 and hue < 45): case = 2
+            #case=3 blau
+            if (hue >= 100 and hue < 130): case = 3
+
+
+            if case == 1: self.__pick_and_place_cli(PickAndPlaceRequest.CASE_1)
+            if case == 2: self.__pick_and_place_cli(PickAndPlaceRequest.CASE_2)
+            if case == 3: self.__pick_and_place_cli(PickAndPlaceRequest.CASE_3)
 
             self.__reset()
 
